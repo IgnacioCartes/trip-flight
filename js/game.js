e2c5eb936e359744a91c66810c32f3cc57e8b77b
Ignacio Cartes <ignacio.cartes.w@gmail.com>
2017-10-27 17:47:40 -0300

    Added basic best time tracking

--- js/game.js
+++ js/game.js
@@ -1,20 +1,20 @@
 var GAME = (function () {
     'use strict';
-    
-    
+
+
     /*
      * GAME VARIABLES
      */
-    
+
     // reference to this instance
     var gameInstance;
-    
+
     // (a very basic) display and timing module - managed by hyne.js
     var display;
-    
+
     // game mode - different scenes within the game
     var mode;
-    
+
     // fade - handles fade in/outs for game mode transitions
     var fade = {
         active: false,
@@ -24,66 +24,68 @@ var GAME = (function () {
         maxCounter: 5,
         callback: null
     };
-    
+
     // key used to access local storage
     var localStorageKey = "com.trip-flight";
-    
-    
+
+
 
     /*
-     * public constructor()
+     * public constructor ()
      *
      *  initializes the game and its main elements (display and game modes)
      *
      */
     var game = function () {
-        
+
         // save a reference to this game instance
         gameInstance = this;
-        
+
         // set size
         this.width = 640;
         this.height = 360;
-        
+
         // Create and configure display
         display = new HYNE(this.width, this.height)
             .appendTo(document.body);
-        
+
         // Set update and render methods
         display.setUpdate(updateWrapper)
             .setRender(renderWrapper);
-        
+
         // Set initial mode
-        this.setMode("TITLE", { nofade: true });
-        
+        this.setMode("TITLE", {
+            nofade: true
+        });
+
         // Initialize the display
         display.run();
-        
+
         // share save data
         this.save = loadLocalStorageData();
-        
+
         // set initial fadein
         fade.active = true;
         fade.direction = 1;
         fade.step = 0;
-        
-        
+
+
     };
-    
-    
-    
+
+
+
     /*
-     * public void .setMode(newMode, argsObj)
+     * public void .setMode (newMode, argsObj)
      *
      *  sets a new game mode, passing an object containing custom arguments to the new game mode
      *
      */
     game.prototype.setMode = function (newMode, argsObj) {
-        
+
         console.log("setting mode " + newMode);
-        
+
         argsObj = argsObj || {};
-        
+
         if (GAME.MODE[newMode]) {
             if (argsObj.nofade) {
                 // nofade mode - set mode right away
@@ -93,12 +95,12 @@ var GAME = (function () {
                 // fade mode - set via fade out -(mode change)-> fade in
                 // first, prevent a mode change if a fadeout is already taking place
                 if (fade.active) return null;
-                
+
                 // set and initiate the fade cycle with the callback to change mode in between
                 fade.active = true;
                 fade.direction = -1;
                 fade.step = 4;
-                fade.callback = function() {
+                fade.callback = function () {
                     fade.active = true;
                     fade.direction = 1;
                     fade.step = 0;
@@ -109,9 +111,9 @@ var GAME = (function () {
             }
         }
     };
-    
-    
-    
+
+
+
     /*
      * public number .getTicks()
      *
@@ -121,9 +123,9 @@ var GAME = (function () {
     game.prototype.getTicks = function () {
         return display.getTicks();
     };
-    
-    
-    
+
+
+
     /*
      * public bool .isFade()
      *
@@ -133,9 +135,9 @@ var GAME = (function () {
     game.prototype.isFade = function () {
         return fade.active;
     };
-    
-    
-    
+
+
+
     /*
      * private void updateWrapper(input)
      *
@@ -143,10 +145,10 @@ var GAME = (function () {
      *
      */
     function updateWrapper(input) {
-        
+
         // call update method depending on mode
         GAME.MODE[mode].update(input);
-        
+
         // handle fade in/outs
         if (fade.active) {
             fade.counter++;
@@ -155,7 +157,7 @@ var GAME = (function () {
                 fade.counter = 0;
                 fade.opacityChange = true;
                 fade.step += fade.direction;
-                
+
                 // check if fade needs to end
                 if ((fade.step === 0) || (fade.step === 4)) {
                     fade.active = false;
@@ -167,11 +169,11 @@ var GAME = (function () {
                 }
             }
         }
-        
+
     }
-    
-    
-    
+
+
+
     /*
      * private void renderWrapper(context)
      *
@@ -179,27 +181,27 @@ var GAME = (function () {
      *
      */
     function renderWrapper(context) {
-        
+
         // clear buffer before calling individual function for each mode
         this.clearBuffer();
-        
+
         // set alpha depending on fade status
         context.globalAlpha = fade.step / 4;
-        
+
         // set default color and font
         context.fillStyle = "#000000";
         context.font = "16px EarlyGameboy";
-        
+
         // call render method depending on mode
         GAME.MODE[mode].render(context);
-        
+
         // Display screen resolution
         context.fillText(window.innerWidth.toString() + ", " + window.innerHeight.toString(), 64, 64);
-        
+
     }
-    
-    
-    
+
+
+
     /*
      * private object loadLocalStorageData()
      *
@@ -207,15 +209,23 @@ var GAME = (function () {
      *
      */
     function loadLocalStorageData() {
-        
+
         var data = window.localStorage.getItem(localStorageKey);
-        
+        //var data;   // used to create new localStorage
+
         if (!data) {
             // no data in localStorage -- initialize an object
             data = {
                 levelsUnlocked: 1,
                 bestTimes: [
-                    500
+                    {
+                        date: null,
+                        time: 99999
+                    },
+                    {
+                        date: null,
+                        time: 99999
+                    },
                 ],
                 saveDataCreationDate: new Date()
             };
@@ -226,17 +236,17 @@ var GAME = (function () {
         }
         return data;
     };
-    
-    
-    
+
+
+
     /*
-     * public void saveLocalStorageData(data)
+     * public void saveLocalStorageData (data)
      *
      *  stores game data into local storage
      *
      */
-    game.prototype.saveLocalStorageData = function(data) {
-        
+    game.prototype.saveLocalStorageData = function (data) {
+
         if (typeof data === "string") {
             // if data was stringified already, store it as it is
             window.localStorage.setItem(localStorageKey, data);
@@ -244,12 +254,39 @@ var GAME = (function () {
             // otherwise, stringify before storing
             window.localStorage.setItem(localStorageKey, JSON.stringify(data));
         }
-        
+
     }
-    
-    
-    
+
+
+
+    /*
+     * public void reportRaceTime (raceTime, level)
+     *
+     *  takes race time obtained during a race and tracks whether a new record was achieved
+     *
+     */
+    game.prototype.reportRaceTime = function (raceTime, level) {
+
+        var saveLevelId = level - 1;
+
+        console.log(raceTime, saveLevelId);
+        // compare racetime with record
+        if (raceTime < this.save.bestTimes[saveLevelId].time) {
+            // new record!
+            this.save.bestTimes[saveLevelId].time = raceTime;
+            this.save.bestTimes[saveLevelId].date = new Date();
+            this.saveLocalStorageData(this.save);
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+
+
     // Return object to global namespace
     return game;
-    
+
 }());
--- js/mode/play.js
+++ js/mode/play.js
@@ -1,63 +1,64 @@
-GAME.MODE = (function(mode) {
+GAME.MODE = (function (mode) {
     'use strict';
-    
-    
+
+
     /*
      * GAME MODE VARIABLES
      */
-    
+
     // module
     var play = {};
-    
+
     // reference to main game object
     var game;
-    
+
     // yacopu - managed by yacopu.js
     var yacopu;
-    
+
     // level layout - managed by level.js
     var level;
-    
+
     // horizontal scrolling
     var scrollX = 0;
-    
+
     // time-related variables
     var raceTime, countdownTime, initialTimeStamp;
-    var hasRaceStarted = false;
-    
+    var hasRaceStarted = false,
+        hasRaceEnded = false;
+
     // flap history
     var flapHistory = [];
-    
+
     // collection of particles
     var particles = [];
-    
+
     // buttons
     var buttons = [];
-    
-    
-    
+
+
+
     /*
      * public void .set(mainGameObj, args)
      *
      *  initializes this game mode
      *
      */
-    play.set = function(mainGameObj, args) {
+    play.set = function (mainGameObj, args) {
         // Set methods will always pass the main game object as an argument first
         // Here we catch it to have acces to it later
         if (game === undefined) game = mainGameObj;
-        
+
         // Create specific level
         level = new GAME.LEVEL(args.level);
-        
+
         // Create yacopu
         yacopu = new GAME.YACOPU();
         yacopu.level = level;
-        
+
         // initialize buttons
         buttons.restart = new GAME.BUTTON(640 - 32 - 16, 16, 32, 32);
         buttons.restart.image = new Image();
-        
+
         // Initialize game variables
         raceTime = 0;
         countdownTime = (60 * 4) - 1;
@@ -65,56 +66,53 @@ GAME.MODE = (function(mode) {
         hasRaceStarted = false;
         flapHistory = [];
     };
-    
-    
-    
+
+
+
     /*
      * public void .update(input)
      *
      *  runs update logic on each frame
      *
      */
-    play.update = function(input) {
-        
+    play.update = function (input) {
+
         // update particles
         GAME.PARTICLE.updateAll(particles, game);
-        
+
         // run particle generators for level if any
-        GAME.PARTICLE.runParticleGenerators(particles, game, level.particleGenerator, { scrollX: scrollX });
-        
-        // create random "cloud" particles for testing every 16 frames
-        //if (game.getTicks() % 16 == 0) {
-        //    particles.push(new GAME.PARTICLE(game.width + scrollX, 2 * Math.round(Math.random() * 64 + 32), { template: "cloud" }));
-        //};
-        
+        GAME.PARTICLE.runParticleGenerators(particles, game, level.particleGenerator, {
+            scrollX: scrollX
+        });
+
         // do nothing more until actual race has started
         if (!hasRaceStarted) {
             // count every 60 frames
             if ((countdownTime % 60) === 0)
                 console.log(countdownTime / 60);
-            
+
             // when countdown hits 0, start race
             if (countdownTime-- <= 0)
                 hasRaceStarted = true;
-            
+
             return null;
         };
-        
+
         // update yacopu movement
         yacopu.update(game);
-        
-        
+
+
         // flap if a touch occured on this frame
         if (input.touch.click) {
             yacopu.flap(game);
-            
+
             // track "flap history"
             if (!yacopu.goal) {
                 flapHistory.push(raceTime);
-            }// else {
+            } // else {
             //    console.log(JSON.stringify(flapHistory));
             //}
-            
+
             // create new random particles
             /*
             for (var i = 0; i < 4; i++) {
@@ -122,66 +120,84 @@ GAME.MODE = (function(mode) {
             };
             */
         }
-        
+
         // count a bonk
         if (yacopu.isBonking) {
             // create particles
-            particles.push(new GAME.PARTICLE(yacopu.x + 32, yacopu.y + 16, { template: "bonkstar", y: -2 }));
-            particles.push(new GAME.PARTICLE(yacopu.x + 32, yacopu.y + 16, { template: "bonkstar", y: 2 }));
+            particles.push(new GAME.PARTICLE(yacopu.x + 32, yacopu.y + 16, {
+                template: "bonkstar",
+                y: -2
+            }));
+            particles.push(new GAME.PARTICLE(yacopu.x + 32, yacopu.y + 16, {
+                template: "bonkstar",
+                y: 2
+            }));
         }
-        
+
         // screen scroll
         if ((yacopu.x - scrollX) > 240) scrollX = Math.floor(yacopu.x - 240);
-            
+
         // Make sure we don't scroll TOO much though
         if (scrollX > level.rightBound) scrollX = level.rightBound;
-        
-        // Increase race time
-        if (!yacopu.goal) {
-            raceTime++;
+
+        // Track race progress
+        if (!hasRaceEnded) {
+
+            if (yacopu.goal) {
+                // If yacopu has reached the goal, then race ended
+                hasRaceEnded = true;
+                // let game object deal with raceTime
+                if (game.reportRaceTime(raceTime, level.id)) {
+
+                    console.log("NEW RECORD!");
+
+                };
+            } else {
+                // increase race time
+                raceTime++;
+            }
         }
-        
+
         // update buttons
         buttons.restart.update(input);
-        if (buttons.restart.release) {console.log("this is the restart level button");}
+        if (buttons.restart.release) {
+            console.log("this is the restart level button");
+        }
     };
-    
-    
-    
+
+
+
     /*
      * public void .render(context)
      *
      *  draws elements to the main canvas
      *
      */
-    play.render = function(context) {
+    play.render = function (context) {
         // display level and yacopu
         level.render(context, scrollX);
         yacopu.render(context, scrollX);
-        
+
         // display button
         buttons.restart.render(context, "#FF0000");
-        
+
         // display particles
         GAME.PARTICLE.renderAll(particles, context, scrollX);
-        
-        // get race time
-        var roundraceTime = Math.round((raceTime / 60) * 100) / 100;
-        
+
         context.fillStyle = "#99BBCC";
-        context.fillText((roundraceTime).toString(), 512, 16);
-        
+        context.fillText((raceTime / 60).toFixed(2), 512, 16);
+
         if (yacopu.goal) {
             context.fillText("max speed: " + yacopu.maxSpeed.toString(), 32, 208);
             context.fillText("bonks: " + yacopu.bonks.toString(), 32, 224);
         }
-        
+
     };
-    
-    
-    
+
+
+
     // Return object to global namespace
     mode.PLAY = play;
     return mode;
-    
+
 }(GAME.MODE || {}));
